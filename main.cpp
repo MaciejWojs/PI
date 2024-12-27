d79ed653f4933546e1727acaaa9bfe4a8e39f08e
Maciej Wójs <misiek111403@gmail.com>
2024-12-27 21:08:46 +0100

    Benchmark argument

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 #include <limits>
+#include <string>
+#include <fstream>
 #include "PI.hpp"
 #define MAX_THREADS 50
+#define FILENAME "benchmark.csv"
 
 int get_num_threads();
+void save_to_csv(int threads, long long points, double result);
 
 int main(int argc, char** argv) {
-    int num_threads = get_num_threads();
+    if (argc > 1 && std::string(argv[1]) == "--benchmark") {
+        long long num_points[3] = { 100000000, 1000000000, 3000000000 };
+        for (auto num_point : num_points) {
+            std::cout << "Points: " << num_point << std::endl;
+            for (int i = 1; i <= MAX_THREADS; i++) {
+                PI pi(num_point, i);
+                double result = pi.calculate();
+                std::cout << "Threads: " << i << " Points: " << num_point << " Time: " << result << std::endl;
+                save_to_csv(i, num_point, result);
+            }
+        }
+    } else {
+        int num_threads = get_num_threads();
+        int num_points = 1000000000;
 
-    int num_points = 1000000000;
-    PI pi(num_points, num_threads);
-    std::cout << pi.calculate() << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+        PI pi(num_points, num_threads);
+        std::cout << pi.calculate() << std::endl;
+        std::cout << "Hello, World!" << std::endl;
+        return 0;
+    }
+}
+
+void save_to_csv(int threads, long long points, double result) {
+    std::ofstream file(FILENAME, std::ios::app);
+    file << threads << "," << points << "," << result << std::endl;
 }
 
 int get_num_threads() {
