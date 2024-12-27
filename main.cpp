b34020a7e303ef9df2d1ffe607c5c6059ce5e3cf
Maciej Wójs <misiek111403@gmail.com>
2024-12-27 20:59:50 +0100

    Working program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "PI.hpp"
 
-int main() {
-    PI pi(1000000, 4);
-    pi.calculate();
+int main(int argc, char** argv) {
+    std::cout << "Enter the number of threads: ";
+    int num_threads;
+    std::cin >> num_threads;
 
+    int num_points = 1000000000;
+    PI pi(num_points, num_threads);
+    std::cout << pi.calculate() << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
